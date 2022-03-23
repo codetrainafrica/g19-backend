96c726b8494092710db49ffb39dcd69271b6f60b
Tony <toneyobaze@gmail.com>
2022-03-23 11:01:15 +0000

    add get, post requests

--- src/index.js
+++ src/index.js
@@ -1,13 +1,36 @@
 const express = require("express");
+const res = require("express/lib/response");
+const path = require("path");
 
 const server = express();
 
-const handleHomeRequest = (request, response) => {
-  console.log(request);
-  response.send("<h1>Hello</h1>");
+// const handleHomeRequest = (request, response) => {
+//   let homePath = express.static(
+//     path.join(__dirname, "public", "pages", "home.html")
+//   );
+//   response.sendFile(homePath);
+// };
+
+// const handleAboutRoute = (request, response) => {
+//   const aboutPath = path.join(__dirname, "public", "pages", "about.html");
+//   response.sendFile(aboutPath);
+// };
+
+// server.use("/about", handleAboutRoute);
+// server.use("/", handleHomeRequest);
+
+const handleGamesRoute = (request, response) => {
+  response.json([
+    "COD",
+    "GTA V",
+    "FIFA 14",
+    "God Of War",
+    "Resident Evil",
+    "Candy Crush",
+  ]);
 };
 
-server.use("/", handleHomeRequest);
+server.get("/games", handleGamesRoute);
 
 server.listen(5003, () => {
   console.log("server running on port 5003");
